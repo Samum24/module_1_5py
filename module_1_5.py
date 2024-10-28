b04eaa363eb7b6b016575c340c7e200695df7d52
Samum24 <miwikar@yandex.ru>
2024-10-28 22:14:26 +0200

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,7 @@
 #immutable_var = 1,2,3,4,
 #print (immutable_var)
 #immutable_var[0]=7
-mutable_list=([1,2,3,4],'a')
+mutable_list=[[1,2,3,4],'a']
 print (mutable_list)
 mutable_list[0][3]=6
 print (mutable_list)
